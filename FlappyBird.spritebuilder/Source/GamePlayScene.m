a3241abec9fcc276c27fd3261376ba534bfe5aca
omprakaash <omprakaash@Oms-MacBook-Pro.local>
2014-06-24 06:03:34 +0530

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,12 +9,18 @@
     // your code here
     character=(Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    timesinceobs=0.0f;
     [self addObstacle];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timesinceobs+=delta;
+    if(timesinceobs>2.0f){
+        [self addObstacle];
+        timesinceobs=0.0f;
+    }
 }
 
 // put new methods here
